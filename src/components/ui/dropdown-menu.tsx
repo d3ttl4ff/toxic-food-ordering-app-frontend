fd755d7f1f4fe4f0924ea029d373b1db2490e944
d3ttl4ff <recon.mikasaackerman@gmail.com>
2024-07-09 20:40:03 +0530

    shadcn.ui components update

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -6,7 +6,7 @@ import {
   DotFilledIcon,
 } from "@radix-ui/react-icons"
 
-import { cn } from "@/lib/utils"
+import { cn } from "/src/lib/utils"
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
