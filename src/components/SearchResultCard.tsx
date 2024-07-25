4f715cff62bfea3a3e8a86d007a9ed409c8fd994
d3ttl4ff <recon.mikasaackerman@gmail.com>
2024-07-25 16:25:52 +0530

    updated styling

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -5,6 +5,7 @@ import { Banknote, Clock, Dot } from "lucide-react";
 
 import React, { useEffect, useRef } from "react";
 import { motion, useInView, useAnimation } from "framer-motion";
+import { CardBody, CardContainer, CardItem } from "./uiAceternity/3d-card";
 
 type Props = {
   restaurant: Restaurant;
@@ -51,13 +52,22 @@ const SearchResultCard = ({ restaurant }: Props) => {
       className="grid lg:grid-cols-[2fr_3fr] gap-5 group"
     >
       <Section>
-        <AspectRatio ratio={16 / 6}>
-          <img
-            src={restaurant.imageUrl}
-            alt="restaurant image"
-            className="rounded-md w-full h-full object-cover"
-          />
-        </AspectRatio>
+        <CardContainer className="inter-var">
+          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+            <CardItem
+              translateZ="50"
+              className="text-xl font-bold text-neutral-600 dark:text-white"
+            >
+              <AspectRatio ratio={16 / 6}>
+                <img
+                  src={restaurant.imageUrl}
+                  alt="restaurant image"
+                  className="rounded-md w-full h-full object-cover"
+                />
+              </AspectRatio>
+            </CardItem>
+          </CardBody>
+        </CardContainer>
       </Section>
       <div>
         <Section center>
