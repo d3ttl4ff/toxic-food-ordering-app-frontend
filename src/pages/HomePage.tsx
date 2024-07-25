84e8393d4c0bfd043a6e65c40ad3d9540c71ba62
d3ttl4ff <recon.mikasaackerman@gmail.com>
2024-07-25 11:24:59 +0530

    updated styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -21,6 +21,9 @@ import toffee from "../assets/cuisineImages/toffeeLime.webp";
 import windowvamp from "../assets/cuisineImages/windowLime.webp";
 import { Separator } from "@/components/ui/separator";
 
+import React, { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+
 export default function HomePage() {
   // const navigate = useNavigate();
 
@@ -47,64 +50,112 @@ export default function HomePage() {
     windowvamp,
   ];
 
+  type SectionProps = {
+    children: React.ReactNode;
+    center?: boolean;
+  };
+
+  const Section = ({ children }: SectionProps, center = false) => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+    const controls = useAnimation();
+
+    useEffect(() => {
+      if (isInView) {
+        controls.start("visible");
+      }
+    }, [isInView]);
+
+    const revealVariants = {
+      hidden: { opacity: 0, y: 75 },
+      visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.25 } },
+    };
+
+    return (
+      <motion.div
+        ref={ref}
+        variants={revealVariants}
+        initial="hidden"
+        animate={controls}
+        className={center ? "flex justify-center items-center" : ""}
+      >
+        {children}
+      </motion.div>
+    );
+  };
+
   return (
     <div className="flex flex-col gap-12">
-      <div className="md:px-32 relative bg-limeTheme-base_500 dark:backdrop-blur-3xl dark:bg-white/10 rounded-lg shadow-md py-8 text-center">
-        <div className="z-10 flex flex-col gap-5">
-          <h1 className="relative text-3xl md:text-5xl font-bold tracking-tight text-white dark:text-limeTheme-base_500">
-            Give in to your toxic snack today!
-          </h1>
-          <span className="text-xl">Food is just a heart attack away!</span>
-          {/* <SearchBar
-            placeholder="Search by City or Town"
-            onSubmit={handleSearchSubmit}
-          /> */}
+      <Section>
+        <div className="md:px-32 relative bg-limeTheme-base_500 dark:backdrop-blur-3xl dark:bg-white/10 rounded-lg shadow-md py-8 text-center">
+          <div className="z-10 flex flex-col gap-5">
+            <h1 className="relative text-3xl md:text-5xl font-bold tracking-tight text-white dark:text-limeTheme-base_500">
+              Give in to your toxic snack today!
+            </h1>
+            <span className="text-xl">Food is just a heart attack away!</span>
+            {/* <SearchBar
+              placeholder="Search by City or Town"
+              onSubmit={handleSearchSubmit}
+            /> */}
+          </div>
+          <BackgroundBeams />
         </div>
-        <BackgroundBeams />
-      </div>
+      </Section>
 
-      <div className="flex flex-col items-center gap-5 my-8">
-        <div className="text-lg mb-5">
-          Now{" "}
-          <span className="text-limeTheme-base_500 hover:font-bold transition-all">
-            available
-          </span>{" "}
-          in these regions!
+      <Section>
+        <div className="flex flex-col items-center gap-5 my-8">
+          <div className="text-lg mb-5">
+            Now{" "}
+            <span className="text-limeTheme-base_500 hover:font-bold transition-all">
+              available
+            </span>{" "}
+            in these regions!
+          </div>
+          <CanvasRevealEffectCard />
         </div>
-        <CanvasRevealEffectCard />
-      </div>
+      </Section>
 
       <Separator />
 
-      <div className="md:px-32 relative bg-limeTheme-selection_base dark:backdrop-blur-3xl dark:bg-white/10 rounded-lg shadow-md py-8 text-center overflow-hidden my-8">
-        <div className="flex gap-3 items-center justify-center">
-          {cuisineImageList.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt="cuisine image"
-              className="w-20 h-20 object-cover"
-            />
-          ))}
+      <Section>
+        <div className="md:px-32 relative bg-limeTheme-selection_base dark:backdrop-blur-3xl dark:bg-white/10 rounded-lg shadow-md py-8 text-center overflow-hidden my-8">
+          <div className="flex gap-3 items-center justify-center">
+            {cuisineImageList.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt="cuisine image"
+                className="w-20 h-20 object-cover"
+              />
+            ))}
+          </div>
+          <BackgroundBeams />
         </div>
-        <BackgroundBeams />
-      </div>
+      </Section>
 
       <div className="grid md:grid-cols-2 gap-5 mb-8">
-        <img src={landingImage} alt="landing image" />
+        <Section>
+          <img src={landingImage} alt="landing image" />
+        </Section>
         <div className="flex flex-col items-center justify-center gap-4 text-center">
-          <span className="font-bold text-3xl tracking-tighter">
-            Reach your dose of toxins even faster!
-          </span>
-          <span>
-            Download the ToxicEats App for faster ordering and personilized
-            kidney failure recommendations
-          </span>
-          <img
-            src={appDownload}
-            alt="app download image"
-            className="w-2/3 h-auto"
-          />
+          <Section>
+            <span className="font-bold text-3xl tracking-tighter">
+              Reach your dose of toxins even faster!
+            </span>
+          </Section>
+          <Section>
+            <span>
+              Download the ToxicEats App for faster ordering and personilized
+              kidney failure recommendations
+            </span>
+          </Section>
+          <Section center>
+            <img
+              src={appDownload}
+              alt="app download image"
+              className="w-2/3 h-auto"
+            />
+          </Section>
         </div>
       </div>
     </div>
